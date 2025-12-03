4b7fa4cbd61cefa20e196cdd342f7e9004bb261e
mohamad1213 <hatami391998@gmail.com>
2025-12-03 12:27:15 +0700

    Update

--- zeta/urls.py
+++ zeta/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('zetaapp.urls')),
 ]
+
+if settings.DEBUG:
+    # Ini TIDAK BOLEH digunakan di mode produksi (DEBUG=False)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Untuk statik di Development (jika STATICFILES_DIRS digunakan):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
